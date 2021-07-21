c72173b2fb1b1a010e498cfa364e5445d2ca1b7c
shuchi <shuchi.khandelwal@gmail.com>
2021-07-21 09:26:31 -0500

    key_remove

--- Vacationpy/api_keys.py
+++ Vacationpy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "d8f90b9ab38ca4b08855bb3e0badf2a5"
+weather_api_key = ""
 
 # Google API Key
 g_key = "YOUR KEY HERE!"
--- configfile.py
+++
@@ -1,9 +0,0 @@
-# Adding config.py file.
-# OpenWeatherMap API Key
-weather_api_key = "d8f90b9ab38ca4b08855bb3e0badf2a5"
-
- Google API Key
-g_key = "AIzaSyAKn1JU6ijzgHXxxbwnjX3o5bYLmUUq45o"
-
-API_practice.ipynb
-random_numbers.ipynb
