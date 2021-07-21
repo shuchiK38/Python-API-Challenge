98fd2f547b91f5699233faf7adf9c8abe6e10773
shuchi <shuchi.khandelwal@gmail.com>
2021-07-21 08:42:37 -0500

    new folder

--- configfile.py
+++ configfile.py
@@ -1,4 +1,9 @@
 # Adding config.py file.
-API_Key ="d8f90b9ab38ca4b08855bb3e0badf2a5"
+# OpenWeatherMap API Key
+weather_api_key = "d8f90b9ab38ca4b08855bb3e0badf2a5"
+
+ Google API Key
+g_key = "AIzaSyAKn1JU6ijzgHXxxbwnjX3o5bYLmUUq45o"
+
 API_practice.ipynb
 random_numbers.ipynb
